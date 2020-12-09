052274476b876c8f2120892a295dd2e23de9c434
Henry <193541T@mymail.nyp.edu.sg>
2020-12-09 20:39:49 +0800

    Epic anims

--- app/src/main/java/sg/diploma/product/splash/SplashPage.java
+++ app/src/main/java/sg/diploma/product/splash/SplashPage.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 
+import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.view.animation.AnimationUtils;
 
@@ -37,36 +38,43 @@ public final class SplashPage extends Activity {
 
         final DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
         final float factor0 = (float)displayMetrics.widthPixels / 4.0f / 254.0f;
+        Animation androidLogoAnim0 = AnimationUtils.loadAnimation(this, R.anim.android_logo_anim);
+        Animation androidLogoAnim1 = AnimationUtils.loadAnimation(this, R.anim.android_logo_anim);
+        Animation androidLogoAnim2 = AnimationUtils.loadAnimation(this, R.anim.android_logo_anim);
+        Animation androidLogoAnim3 = AnimationUtils.loadAnimation(this, R.anim.android_logo_anim);
 
         androidLogo0 = findViewById(R.id.androidLogo0);
-        androidLogo0.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
+        androidLogo0.startAnimation(androidLogoAnim0);
         androidLogo0.getLayoutParams().width = (int)(254.0f * factor0);
         androidLogo0.getLayoutParams().height = (int)(284.0f * factor0);
 
         androidLogo1 = findViewById(R.id.androidLogo1);
-        androidLogo1.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
+        androidLogoAnim1.setStartOffset(300);
+        androidLogo1.startAnimation(androidLogoAnim1);
         androidLogo1.getLayoutParams().width = (int)(254.0f * factor0);
         androidLogo1.getLayoutParams().height = (int)(284.0f * factor0);
 
         androidLogo2 = findViewById(R.id.androidLogo2);
-        androidLogo2.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
+        androidLogoAnim2.setStartOffset(600);
+        androidLogo2.startAnimation(androidLogoAnim2);
         androidLogo2.getLayoutParams().width = (int)(254.0f * factor0);
         androidLogo2.getLayoutParams().height = (int)(284.0f * factor0);
 
         androidLogo3 = findViewById(R.id.androidLogo3);
-        androidLogo3.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
+        androidLogoAnim3.setStartOffset(900);
+        androidLogo3.startAnimation(androidLogoAnim3);
         androidLogo3.getLayoutParams().width = (int)(254.0f * factor0);
         androidLogo3.getLayoutParams().height = (int)(284.0f * factor0);
 
         final float factor1 = (float)displayMetrics.widthPixels / 513.0f;
         androidStudioLogo = findViewById(R.id.androidStudioLogo);
-        androidStudioLogo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_down));
+        androidStudioLogo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.android_studio_logo_anim));
         androidStudioLogo.getLayoutParams().width = (int)(513.0f * factor1);
         androidStudioLogo.getLayoutParams().height = (int)(512.0f * factor1);
 
         final float factor2 = (float)displayMetrics.heightPixels / 20.0f / 122.0f;
         androidStudioText = findViewById(R.id.androidStudioText);
-        androidStudioText.startAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_up));
+        androidStudioText.startAnimation(AnimationUtils.loadAnimation(this, R.anim.android_studio_text_anim));
         androidStudioText.getLayoutParams().height = (int)(122.0f * factor2);
 
         Thread splashTread = new Thread() {
