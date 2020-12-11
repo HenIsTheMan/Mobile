fc81f5ec252a73eab990d5cbde33789c679a91ba
Henry <193541T@mymail.nyp.edu.sg>
2020-12-11 17:02:30 +0800

    Meh

--- app/src/main/java/sg/diploma/product/activities/GameScreenActivity.java
+++ app/src/main/java/sg/diploma/product/activities/GameScreenActivity.java
@@ -1,14 +1,14 @@
 package sg.diploma.product.activities;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Canvas;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 
 import sg.diploma.product.entity.EntityManager;
+import sg.diploma.product.entity.entities.EntityBG;
+import sg.diploma.product.entity.entities.EntityPlayerChar;
 import sg.diploma.product.game.GameView;
 import sg.diploma.product.state.IState;
 import sg.diploma.product.state.StateManager;
@@ -16,14 +16,16 @@ import sg.diploma.product.touch.TouchManager;
 import sg.diploma.product.touch.TouchTypes;
 
 public final class GameScreenActivity extends Activity implements IState{
-    public static GameScreenActivity Instance = null;
+    public GameScreenActivity(){
+        elapsedTime = 0.0f;
+        entityBG = null;
+        playerChar = null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
         Instance = this;
-
         setContentView(new GameView(this));
     }
 
@@ -33,8 +35,6 @@ public final class GameScreenActivity extends Activity implements IState{
         return true;
     }
 
-    private float timer = 0.0f;
-
     @Override
     public String GetName(){
         return "GameScreen";
@@ -42,8 +42,8 @@ public final class GameScreenActivity extends Activity implements IState{
 
     @Override
     public void OnEnter(SurfaceView _view){
-        //RenderBackground.Create();
-        //Example to include another Renderview for Pause Button
+        //entityBG = EntityBG.Create();
+        //playerChar = EntityPlayerChar.Create();
     }
 
     @Override
@@ -59,10 +59,25 @@ public final class GameScreenActivity extends Activity implements IState{
 
     @Override
     public void Update(float _dt) {
+        elapsedTime += _dt;
         EntityManager.Instance.Update(_dt);
 
         if(TouchManager.Instance.GetMotionEventAction() == TouchTypes.TouchType.Down.GetVal()) {
             StateManager.Instance.ChangeState("MenuScreen");
         }
+
+        EntityManager.Instance.Update(_dt);
+
+    }
+
+    private float elapsedTime;
+
+    private EntityBG entityBG;
+    private EntityPlayerChar playerChar;
+
+    public static GameScreenActivity Instance;
+
+    static{
+        Instance = null;
     }
 }
