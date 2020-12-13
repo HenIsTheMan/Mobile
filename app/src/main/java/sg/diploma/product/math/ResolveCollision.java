77e114fcba4aa98393805852d041b6f3311e746b
Henry <193541T@mymail.nyp.edu.sg>
2020-12-14 07:08:47 +0800

    Update ResolveCollision.java

--- app/src/main/java/sg/diploma/product/math/ResolveCollision.java
+++ app/src/main/java/sg/diploma/product/math/ResolveCollision.java
@@ -15,23 +15,9 @@ public final class ResolveCollision{
 			return; //Return if vel is a zero/null/isotropic vec
 		}
 
-		if(entity.attribs.prevPos.x - myData.halfWidth >= otherData.xMax){
-			entity.attribs.boxColliderPos.x = otherData.xMax + myData.halfWidth;
-			entity.attribs.pos.x = entity.attribs.boxColliderPos.x;
-		}
-		if(entity.attribs.prevPos.x + myData.halfWidth <= otherData.xMin){
-			entity.attribs.boxColliderPos.x = otherData.xMin - myData.halfWidth;
-			entity.attribs.pos.x = entity.attribs.boxColliderPos.x;
-		}
 		if(entity.attribs.prevPos.y - myData.halfHeight <= otherData.yMin){
 			entity.attribs.boxColliderPos.y = otherData.yMin - myData.halfHeight;
 			entity.attribs.pos.y = entity.attribs.boxColliderPos.y - entity.attribs.boxColliderScale.y * 0.075f;
-			entity.attribs.vel.y = 0.0f;
-		}
-		if(entity.attribs.prevPos.y + myData.halfHeight >= otherData.yMax){
-			entity.attribs.boxColliderPos.y = otherData.yMax + myData.halfHeight;
-			entity.attribs.pos.y = entity.attribs.boxColliderPos.y - entity.attribs.boxColliderScale.y * 0.075f;
-			entity.attribs.vel.y = 0.0f;
 		}
 	}
 }
