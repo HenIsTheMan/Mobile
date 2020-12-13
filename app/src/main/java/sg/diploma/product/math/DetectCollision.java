73e097d94c8ba238747896dab910d91f82cfb97a
Henry <193541T@mymail.nyp.edu.sg>
2020-12-13 12:18:37 +0800

    Update DetectCollision.java

--- app/src/main/java/sg/diploma/product/math/DetectCollision.java
+++ app/src/main/java/sg/diploma/product/math/DetectCollision.java
@@ -11,7 +11,15 @@ public final class DetectCollision{
         return distSquared <= rSquared;
     }
 
-    public static boolean AABBAABB(final Vector2 pos0, final Vector2 pos1, final Vector2 scale0, final Vector2 scale1){
-        return false;
+    public static boolean BoxBoxAABB(final Vector2 pos0, final Vector2 pos1, final Vector2 scale0, final Vector2 scale1){
+        float[] halfWidth = new float[]{scale0.x * 0.5f, scale1.x * 0.5f};
+        float[] halfHeight = new float[]{scale0.y * 0.5f, scale1.y * 0.5f};
+
+        float[] xMin = new float[]{pos0.x - halfWidth[0], pos1.x - halfWidth[1]};
+        float[] xMax = new float[]{pos0.x + halfWidth[0], pos1.x + halfWidth[1]};
+        float[] yMin = new float[]{pos0.y - halfHeight[0], pos1.y - halfHeight[1]};
+        float[] yMax = new float[]{pos0.y + halfHeight[0], pos1.y + halfHeight[1]};
+
+        return xMin[0] < xMax[1] && xMax[0] > xMin[1] && yMin[0] < yMax[1] && yMax[0] > yMin[1];
     }
 }
