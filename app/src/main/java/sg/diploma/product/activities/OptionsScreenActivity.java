8351ea9f411188ac9b1e91d04f2f4e5e1c05e3ba
Henry <193541T@mymail.nyp.edu.sg>
2020-12-14 19:35:14 +0800

    Back I guess

--- app/src/main/java/sg/diploma/product/activities/OptionsScreenActivity.java
+++ app/src/main/java/sg/diploma/product/activities/OptionsScreenActivity.java
@@ -1,22 +1,28 @@
 package sg.diploma.product.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.RequiresApi;
 
 import sg.diploma.product.R;
+import sg.diploma.product.audio.AudioManager;
+import sg.diploma.product.device.DeviceManager;
 import sg.diploma.product.entity.EntityManager;
 import sg.diploma.product.state.IState;
+import sg.diploma.product.state.StateManager;
 import sg.diploma.product.touch.TouchManager;
 
 public final class OptionsScreenActivity extends Activity implements View.OnClickListener, IState{
 	public OptionsScreenActivity(){
+		backButton = null;
 	}
 
 	@RequiresApi(api = Build.VERSION_CODES.P)
@@ -25,6 +31,16 @@ public final class OptionsScreenActivity extends Activity implements View.OnClic
 		super.onCreate(savedInstanceState);
 		Instance = this;
 		setContentView(R.layout.options_screen_layout);
+
+		final float buttonFactor = DeviceManager.screenWidthF * 0.1f / 300.0f;
+		final int buttonSize = (int)(300.0f * buttonFactor);
+
+		backButton = findViewById(R.id.backButton);
+		backButton.setOnClickListener(this);
+		backButton.getLayoutParams().width = buttonSize;
+		backButton.getLayoutParams().height = buttonSize;
+		backButton.setTranslationX(buttonSize * 0.5f);
+		backButton.setTranslationY(buttonSize * 0.5f);
 	}
 
 	@Override
@@ -35,6 +51,13 @@ public final class OptionsScreenActivity extends Activity implements View.OnClic
 
 	@Override
 	public void onClick(View v){
+		AudioManager.Instance.PlayAudio(R.raw.button_press, 5);
+		if(v == backButton){
+			EntityManager.Instance.SendAllEntitiesForRemoval();
+			StateManager.Instance.ChangeState("MenuScreen");
+
+			startActivity(new Intent(this, MenuScreenActivity.class));
+		}
 	}
 
 	@Override
@@ -80,6 +103,8 @@ public final class OptionsScreenActivity extends Activity implements View.OnClic
 	protected void onDestroy(){
 		super.onDestroy();
 	}
+
+	private Button backButton;
 
 	public static OptionsScreenActivity Instance;
 
