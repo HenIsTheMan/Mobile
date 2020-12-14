c787b6f73f573da328184bd82c1ebdf7f126f11e
Henry <193541T@mymail.nyp.edu.sg>
2020-12-14 13:39:00 +0800

    EntityPauseButton.java

--- app/src/main/java/sg/diploma/product/entity/entities/EntityPauseButton.java
+++ app/src/main/java/sg/diploma/product/entity/entities/EntityPauseButton.java
@@ -1,6 +1,20 @@
 package sg.diploma.product.entity.entities;
 
-/*
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import sg.diploma.product.BuildConfig;
+import sg.diploma.product.R;
+import sg.diploma.product.audio.AudioManager;
+import sg.diploma.product.entity.EntityAbstract;
+import sg.diploma.product.entity.EntityManager;
+import sg.diploma.product.game.GameManager;
+import sg.diploma.product.graphics.ResourceManager;
+import sg.diploma.product.math.DetectCollision;
+import sg.diploma.product.math.Vector2;
+import sg.diploma.product.touch.TouchManager;
+import sg.diploma.product.touch.TouchTypes;
+
 public final class EntityPauseButton extends EntityAbstract{
 	public EntityPauseButton(final int bitmapID){
 		paused = false;
@@ -9,21 +23,18 @@ public final class EntityPauseButton extends EntityAbstract{
 
 	@Override
 	public void Update(float _dt){
-		if(TouchManager.Instance.GetMotionEventAction() == MotionEvent.I ){
-			if (TouchManager.Instance.IsDown() && !Paused) {   // Check touch collision here
-				float imgRadius = scaledbmpP.getHeight() * 0.5f;
-
-				if (Collision.SphereToSphere(TouchManager.Instance.GetPosX(), TouchManager.Instance.GetPosY(), 0.0f, xPos, yPos, imgRadius)) {
-					Paused = true; // Meant user had pressed the Pause button!!!
-
-					// When button is pressed, U can play an audio clip
-					// AudioManager.Instance.PlayAudio(R.raw.clicksound);
-
-					// If just want a pause without the (popup dialog --> No done yet.)
-					// Method already written in your GameSystem class from Week 5
-					GameSystem.Instance.SetIsPaused(!GameSystem.Instance.GetIsPaused());
-				}
-			}
+		if(TouchManager.Instance.GetMotionEventAction() == TouchTypes.TouchType.Down.GetVal()
+			&& DetectCollision.CircleCircle(new Vector2(
+				TouchManager.Instance.GetXPos(),
+				TouchManager.Instance.GetYPos()),
+				attribs.pos,
+				0.0f,
+				attribs.scale.x * 0.5f
+			)
+		){
+			paused = true;
+			AudioManager.Instance.PlayAudio(R.raw.button_press, 5);
+			GameManager.Instance.SetIsPaused(paused);
 		} else{
 			paused = false;
 		}
@@ -31,12 +42,26 @@ public final class EntityPauseButton extends EntityAbstract{
 
 	@Override
 	public void Render(Canvas _canvas){
-		*/
-/*if (Paused == false)
+		/*if (Paused == false)
 			_canvas.drawBitmap(scaledbmpP, xPos - scaledbmpP.getWidth() * 0.5f, yPos - scaledbmpP.getHeight() * 0.5f, null);
-		else*//*
+		else*/
 
 		//_canvas.drawBitmap(scaledbmpUP, xPos - scaledbmpUP.getWidth() * 0.5f, yPos - scaledbmpUP.getHeight() * 0.5f, null);
+	}
+
+	@Override
+	public void Collided(EntityAbstract other){
+	}
+
+	@Override
+	public void LateUpdate(final float dt){
+	}
+
+	@Override
+	public void SpecialRender(final Canvas canvas){
+		if(BuildConfig.DEBUG){
+			throw new AssertionError("Assertion failed");
+		}
 	}
 
 	public static EntityPauseButton Create(final String key, final int bitmapID){
@@ -45,6 +70,6 @@ public final class EntityPauseButton extends EntityAbstract{
 		return result;
 	}
 
-	private Bitmap bitmap;
+	private final Bitmap bitmap;
 	private boolean paused;
-}*/
+}
