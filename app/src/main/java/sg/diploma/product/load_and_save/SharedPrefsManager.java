1665c1ae497803af2904f73c054e9d3578c3d40c
Henry <193541T@mymail.nyp.edu.sg>
2021-01-05 19:52:45 +0800

    huh

--- app/src/main/java/sg/diploma/product/load_and_save/SharedPrefsManager.java
+++ app/src/main/java/sg/diploma/product/load_and_save/SharedPrefsManager.java
@@ -4,20 +4,44 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.view.SurfaceView;
 
+import java.util.HashMap;
+
 public class SharedPrefsManager{
 	SharedPrefsManager(){
 		view = null;
 	}
 
-	public void Init(final SurfaceView view){
+	public void Init(final SurfaceView view){ //??
 		this.view = view;
 	}
 
 	public void Load(final String name){
-		SharedPreferences sharedPrefs = view.getContext().getSharedPreferences(name, Context.MODE_PRIVATE);
+		//SharedPreferences sharedPrefs = view.getContext().getSharedPreferences(name, Context.MODE_APPEND);
+
+		//                                                          float val = sharedPrefs.getFloat("name", "");
 	}
 
-	public void Save(final String name){
+	public void Save(final String name, final HashMap<String, Object> objs){
+		SharedPreferences sharedPrefs = view.getContext().getSharedPreferences(name, Context.MODE_PRIVATE);
+
+		SharedPreferences.Editor editor = sharedPrefs.edit();
+		Object sample = objs.values().stream().findFirst();
+		final String[] keys = objs.keySet().toArray(new String[0]);
+
+		if(sample instanceof Float){
+			for(final String key: keys){
+				editor.putFloat(key, (float)objs.get(key));
+			}
+			return;
+		}
+
+		if(sample instanceof String){
+			for(final String key: keys){
+				editor.putString(key, (String)objs.get(key));
+			}
+		}
+
+		editor.apply();
 	}
 
 	static SharedPrefsManager Instance;
