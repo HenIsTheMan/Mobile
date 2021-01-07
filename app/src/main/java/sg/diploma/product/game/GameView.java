24b4ed534d9ee30d64594d1a7258389d25c086a8
Henry <193541T@mymail.nyp.edu.sg>
2021-01-07 17:44:01 +0800

    gif BG progress

--- app/src/main/java/sg/diploma/product/game/GameView.java
+++ app/src/main/java/sg/diploma/product/game/GameView.java
@@ -1,14 +1,25 @@
 package sg.diploma.product.game;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Movie;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.io.InputStream;
+
+import sg.diploma.product.R;
 import sg.diploma.product.device.UpdateThread;
 
-public final class GameView extends SurfaceView {
+public final class GameView extends SurfaceView{
     public GameView(final Context context){
         super(context);
+
+        setWillNotDraw(false);
+        final InputStream is = context.getResources().openRawResource(R.raw.game_background);
+        movie = Movie.decodeStream(is);
+        movieStart = 0;
+
         updateThread = new UpdateThread(this);
         SurfaceHolder surfaceHolder = getHolder(); //Holds content
 
@@ -39,5 +50,22 @@ public final class GameView extends SurfaceView {
         }
     }
 
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+
+        final long now = android.os.SystemClock.uptimeMillis();
+        if(movieStart == 0){
+            movieStart = now;
+        }
+
+        final int relTime = (int)((now - movieStart) % movie.duration());
+        movie.setTime(relTime);
+        movie.draw(canvas, (float)getWidth() * 0.5f, (float)getHeight() * 0.5f);
+        invalidate();
+    }
+
     private final UpdateThread updateThread;
+    private final Movie movie;
+    private long movieStart;
 }
