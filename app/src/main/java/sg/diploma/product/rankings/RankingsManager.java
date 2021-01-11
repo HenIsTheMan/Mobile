16bed6285d036761954f8c0afd9c240eb8eca22e
Henry <193541T@mymail.nyp.edu.sg>
2021-01-11 16:03:42 +0800

    ClearDirectory

--- app/src/main/java/sg/diploma/product/rankings/RankingsManager.java
+++ app/src/main/java/sg/diploma/product/rankings/RankingsManager.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Objects;
 
 public class RankingsManager{
 	RankingsManager(){
@@ -93,6 +94,9 @@ public class RankingsManager{
 
 		final String pathToAppFolder = context.getExternalFilesDir(null).getAbsolutePath();
 		final File dir = new File(pathToAppFolder + File.separator);
+		if(dir.exists()){
+			ClearDirectory(dir);
+		}
 		dir.mkdirs();
 
 		final File scoresFile = new File(dir, scoresFileName);
@@ -144,6 +148,16 @@ public class RankingsManager{
 
 	public TreeMultimap<Integer, String> GetRankings(){
 		return rankings;
+	}
+
+	/** @noinspection ResultOfMethodCallIgnored*/
+	private void ClearDirectory(final File file){
+		if(file.isDirectory()){
+			for(File child: Objects.requireNonNull(file.listFiles())){
+				ClearDirectory(child);
+			}
+		}
+		file.delete();
 	}
 
 	private final TreeMultimap<Integer, String> rankings;
